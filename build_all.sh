497175c027a5f17c56339cd95fd62cc42a408c5a
Mokka <mokkamichi.99@gmail.com>
2019-10-27 13:35:10 +0100

    should be just about fine

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 a_head="<a href='"
 a_mid=".pdf'>"
-a_tail="</a>\n"
+a_tail="</a><br/>\n"
 
 cat index_h.html > public/songs/index.html
 
@@ -13,8 +13,8 @@ do
 #    xelatex temp.tex
 #    xelatex temp.tex
 #    mv temp.pdf ../public/songs/${f%%.*}.pdf
-    echo $a_head$f$a_mid${f%%.*}$a_tail
-    echo $a_head$f$a_mid${f%%.*}$a_tail >> ../public/songs/index.html
+    echo $a_head${f%%.*}$a_mid$f$a_tail
+    echo $a_head${f%%.*}$a_mid$f$a_tail >> ../public/songs/index.html
 done
 
 cd ..
