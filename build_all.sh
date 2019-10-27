e643125ca30b415cfbfdb0c8c031e139c5208df4
Mokka <mokkamichi.99@gmail.com>
2019-10-27 13:21:09 +0100

    More ci, I'm daring today c;

--- build_all.sh
+++ build_all.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 shopt -s nullglob
+
+cat index_h.html > public/songs/index.html
+
 cd songs
 for f in *.tex
 do
-    cat ../header.tex $f ../footer.tex > temp.tex
-    xelatex temp.tex
-    xelatex temp.tex
-    xelatex temp.tex
-    mv temp.pdf ../public/songs/${f%%.*}.pdf
+#    cat ../header.tex $f ../footer.tex > temp.tex
+#    xelatex temp.tex
+#    xelatex temp.tex
+#    xelatex temp.tex
+#    mv temp.pdf ../public/songs/${f%%.*}.pdf
+    echo '<a href="${f%%.*}.pdf">$f</a>' >> ../public/songs/index.html
 done
+
+cd ..
+cat index_f.html >> public/songs/index.html
