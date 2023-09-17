e3f4acb7b48f784bf76f4cdfbe951b3007a4b4f7
Mokka <mokkamichi.99@gmail.com>
2023-09-17 14:05:18 +0200

    replace old gitlab ci with github actions (maybe)

--- build_all.sh
+++ build_all.sh
@@ -3,7 +3,7 @@ a_head="\t\t\t<li><a target='_blank' href='songs/"
 a_mid=".pdf'>"
 a_tail="</a></li>\n"
 
-cat index_h.html > public/index.html
+cat index_h.html > _site/index.html
 
 cd songs
 for f in *.tex
@@ -12,9 +12,9 @@ do
     xelatex temp.tex
     xelatex temp.tex
     xelatex temp.tex
-    mv temp.pdf ../public/songs/${f%%.*}.pdf
-    echo $a_head${f%%.*}$a_mid${f%%.*}$a_tail >> ../public/index.html
+    mv temp.pdf ../_site/songs/${f%%.*}.pdf
+    echo $a_head${f%%.*}$a_mid${f%%.*}$a_tail >> ../_site/index.html
 done
 
 cd ..
-cat index_f.html >> public/index.html
+cat index_f.html >> _site/index.html
